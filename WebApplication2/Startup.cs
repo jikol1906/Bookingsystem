9cdfd83cd2350ac607c6f33512341a640b0abf6a
Boris Grunwald <borisgrunwald@gmail.com>
2018-05-01 12:06:41 +0200

    added folders

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -29,7 +29,7 @@ namespace WebApplication2
             
            
 
-            app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
+            //app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
         }
     }
 }
